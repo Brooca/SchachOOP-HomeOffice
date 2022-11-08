0542d6761365bb4f8049546186c1dfc2ed2a7acf
larbaer <lorbaer@posteo.de>
2022-11-08 15:38:39 +0100

    Dame Turm Bezeichnung

--- fm1SchachOOP/Dame.cpp
+++ fm1SchachOOP/Dame.cpp
@@ -4,4 +4,7 @@ Dame::Dame(bool f)
 {
 	farbe = f;
 	std::cout << "Farbe Dame: " << f << std::endl;
+
+	bezeichnung = (f) ? 'D' : 'd';
+	std::cout << "bezeichnung : " << bezeichnung << std::endl;
 }
--- fm1SchachOOP/Turm.cpp
+++ fm1SchachOOP/Turm.cpp
@@ -2,4 +2,7 @@
 Turm::Turm(bool symbol) {
 	farbe = symbol;  // true or false 
 	std::cout << "Farbe Turm : " << farbe << std::endl;
+
+	bezeichnung = (symbol) ? 'T' : 't';
+	std::cout << "bezeichnung : " << bezeichnung << std::endl;
 }
