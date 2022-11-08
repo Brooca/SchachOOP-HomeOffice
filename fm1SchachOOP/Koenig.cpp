726b1199a2a19a25c566395a5662d319dd5e99dc
Manuela Vasco <MVAS170@more2learn.de>
2022-11-08 15:30:19 +0100

    Koenig update

--- fm1SchachOOP/Koenig.cpp
+++ fm1SchachOOP/Koenig.cpp
@@ -4,5 +4,7 @@ Koenig::Koenig(bool symbol)
 {
 	std::cout << "Koenig instanziert\n";
 	farbe = symbol;
+	bezeichnung = (symbol) ? 'B' : 'b';
 	std::cout << "symbol : " << symbol << std::endl;
+	std::cout << "bezeichnung : " << bezeichnung << std::endl;
 }
