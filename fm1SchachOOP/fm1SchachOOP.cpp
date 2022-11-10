413993da6bb49ed1c0bf474e3c753cafa6a8f372
larbaer <lorbaer@posteo.de>
2022-11-10 08:15:01 +0100

    Layout main

--- fm1SchachOOP/fm1SchachOOP.cpp
+++ fm1SchachOOP/fm1SchachOOP.cpp
@@ -1,12 +1,8 @@
 // fm1SchachOOP.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
 
 #include <iostream>
-
 #include "Spiel.h"
 #include "Anzeige.h"
-
-
 
 int main()
 {
