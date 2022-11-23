cddb57f8e360ef9a3fc83dc76d7f9bb46c54168d
Brooca <53477954+Brooca@users.noreply.github.com>
2022-11-23 22:55:44 +0100

    nice

--- fm1SchachOOP/Turm.cpp
+++ fm1SchachOOP/Turm.cpp
@@ -20,23 +20,23 @@ vector<Feld> Turm::erlaubteFelderBerechnen(string bezeichnung)
 				for (int k = 0; k < 8; k++)
 				{
 					// Berechnung der erlaubten Felder 								
-					if (bezeichnung.at(m) == (65 + i - (16 * m)))												// Feld auf dem man steht				| 65 + 0 = A 		
+					if (bezeichnung.at(m) == (65 + i - (16 * m)))													// Feld auf dem man steht				| 65 + 0 = A 		
 					{
 						string x	 = "  ";
-						x.at(m)		 = bezeichnung.at(m) + k - (k * n);											// Feld auf das man sich bewegen kann	| 65 + 0 = A 		
-						x.at(1 - m)  = bezeichnung.at(1 - m);													// vertikale bleibt unverändert		
-						if (x.at(m) != (65 + i - (16 * m)) && x.at(m) >= 65 - (16 * m) && x.at(m) <= 72 - (16 * m)) 						// Felder auf dem man steht excludieren & nur on Board range erlauben		
+						x.at(m)		 = bezeichnung.at(m) + k - (k * n);												// Feld auf das man sich bewegen kann	| 65 + 0 = A 		
+						x.at(1 - m)  = bezeichnung.at(1 - m);														// vertikale / horizontale bleibt unverändert		
+						if (x.at(m) != (65 + i - (16 * m)) && x.at(m) >= 65 - (16 * m) && x.at(m) <= 72 - (16 * m)) // Felder auf dem man steht excludieren & nur on Board range erlauben		
 						{
-							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')					// Wenn das Feld leer ist, ist es erlaubt -> pushback
+							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')						// Wenn das Feld leer ist, ist es erlaubt -> pushback
 							{
 								TurmV.push_back(x);
 
 							}
-							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())				// Wenn die Figur auf dem Feld die selbe Farbe hat als diese Figur kein pushback & break
+							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())					// Wenn die Figur auf dem Feld die selbe Farbe hat als diese Figur kein pushback & break
 							{
 								break;
 							}
-							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())				// Wenn die Figur auf dem Feld eine andere Farbe hat als diese Figur, ist es erlaubt -> pushback & danach break
+							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())					// Wenn die Figur auf dem Feld eine andere Farbe hat als diese Figur, ist es erlaubt -> pushback & danach break
 							{
 								TurmV.push_back(x);
 								break;
