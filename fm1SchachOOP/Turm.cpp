78c2477c064741161a415c8a6226b3ec1b87a291
larbaer <lorbaer@posteo.de>
2022-11-23 10:57:20 +0100

    Turm seperating vertical and horizontal

--- fm1SchachOOP/Turm.cpp
+++ fm1SchachOOP/Turm.cpp
@@ -11,27 +11,62 @@ vector<Feld> Turm::erlaubteFelderBerechnen(string bezeichnung)
 {
 	// Calculation
 	vector<string> TurmV;
+
+	// Berechnung der horizontal erlaubten Felder
+
+	// Rechts
 	for (int i = 0; i < 8; i++)
 	{
-		for(int k = 7; k >= 0; k--)
+		for (int k = 7; k >= 0; k--)
 		{
-		if (bezeichnung.at(0) == (65 + i))
-			{
 			string x = "  ";
-				x.at(0) = bezeichnung.at(0) + k - i;
-				x.at(1) = bezeichnung.at(1);
-				if (x.at(0) != (65 + i))
-				{
-					TurmV.push_back(x);  
+			x.at(0) = bezeichnung.at(0);
+			// erster durchlauf der aeusseren Schleife	//letzter durchlauf der aeusseren Schleife
+			if ((bezeichnung.at(0) == (65 + i)) && x.at(0) < 'I')	// wenn Spielfigur auf A + 0 = A										// wenn Spielfigur auf A + 7 = H
+			{
+														// erster durchgang der inneren // letzte durchgang der inneren			// erster durchgang der inneren // letzte durchgang der inneren
+				x.at(0) = bezeichnung.at(0)++;			// A + 7 - 0 = H		 		// A + 0 - 0 = A 						// H + 7 - 7 = H		 		// H + 0 - 7 = A 
+				x.at(1) = bezeichnung.at(1);			// Zahl bleibt unverändert
+				if (x.at(0) != (65 + i))				// wenn die ausgerechnete zahl also A + k - i == A + i entspricht, dann nicht ausgeben  
+				{										// damit wird die Position excludiert, auf der die Figur steht
+					TurmV.push_back(x);
 				}
 			}
-			if (bezeichnung.at(1) == (49 + i))
+		}
+	}
+
+	// Links
+	for (int i = 0; i < 8; i++)
+	{
+		for (int k = 7; k >= 0; k--)
+		{
+			// erster durchlauf der aeusseren Schleife		//letzter durchlauf der aeusseren Schleife
+			if (bezeichnung.at(0) == (65 + i))				// wenn Spielfigur auf A + 0 = A										// wenn Spielfigur auf A + 7 = H
 			{
-				string x = "  ";
-				x.at(0) = bezeichnung.at(0) ;
-				x.at(1) = bezeichnung.at(1) + k - i;
-				if (x.at(1) != (49 + i))
-				{
+				string x = "  ";							// erster durchgang der inneren // letzte durchgang der inneren			// erster durchgang der inneren // letzte durchgang der inneren
+				x.at(0) = bezeichnung.at(0) + k - i;	// A + 7 - 0 = H		 		// A + 0 - 0 = A 						// H + 7 - 7 = H		 		// H + 0 - 7 = A 
+				x.at(1) = bezeichnung.at(1);			// Zahl bleibt unverändert
+				if (x.at(0) != (65 + i))				// wenn die ausgerechnete zahl also A + k - i == A + i entspricht, dann nicht ausgeben  
+				{										// damit wird die Position excludiert, auf der die Figur steht
+					TurmV.push_back(x);
+				}
+			}
+		}
+	}
+
+	// Berechnung der vertikal erlaubten Felder	
+	for (int i = 0; i < 8; i++)
+	{
+		for (int k = 7; k >= 0; k--)
+		{
+			// erster durchlauf der aeusseren Schleife								//letzter durchlauf der aeusseren Schleife
+			if (bezeichnung.at(1) == (49 + i))			// wenn Spielfigur auf 1 + 0 = 1										// wenn Spielfigur auf 1 + 7 = 8
+			{
+				string x = "  ";						// erster durchgang der inneren // letzte durchgang der inneren			// erster durchgang der inneren // letzte durchgang der inneren
+				x.at(0) = bezeichnung.at(0);			// Buchstabe bleibt unverändert
+				x.at(1) = bezeichnung.at(1) + k - i;	// 1 + 7 - 0 = 8		 		// 1 + 0 - 0 = 1 						// 8 + 7 - 7 = 8		 		// 8 + 0 - 7 = 1 
+				if (x.at(1) != (49 + i))				// wenn die ausgerechnete zahl also 1 + k - i == 1 + i entspricht, dann nicht ausgeben 
+				{										// damit wird die Position excludiert, auf der die Figur steht			
 					TurmV.push_back(x);
 				}
 			}
@@ -41,6 +76,6 @@ vector<Feld> Turm::erlaubteFelderBerechnen(string bezeichnung)
 	for (string& str : TurmV) {
 		felder.push_back(s.get_Spielstand()[str]);
 	}
-	
+
 	return felder;
 }
