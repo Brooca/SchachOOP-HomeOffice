33fa1af02cf87818786f6c4262687d96e432166b
Brooca <53477954+Brooca@users.noreply.github.com>
2022-11-23 10:20:52 +0100

    Turm Kommentare

--- fm1SchachOOP/Laeufer.cpp
+++ fm1SchachOOP/Laeufer.cpp
@@ -6,8 +6,38 @@ Laeufer::Laeufer(bool symbol) {
 }
 
 vector<Feld> Laeufer::erlaubteFelderBerechnen(string bezeichnung)
-{/*
-	// Test - Value
-	return {"X1","X2","X3"};*/
+{
+		// Calculation
+		vector<string> LaeuferV;
+		for (int i = 0; i < 8; i++)
+		{
+			for (int k = 7; k >= 0; k--)
+			{
+				if (bezeichnung.at(0) == (65 + i))
+				{
+					string x = "  ";
+					x.at(0) = bezeichnung.at(0) + k - i;
+					x.at(1) = bezeichnung.at(1);
+					if (x.at(0) != (65 + i))
+					{
+						LaeuferV.push_back(x);
+					}
+				}
+				if (bezeichnung.at(1) == (49 + i))
+				{
+					string x = "  ";
+					x.at(0) = bezeichnung.at(0);
+					x.at(1) = bezeichnung.at(1) + k - i;
+					if (x.at(1) != (49 + i))
+					{
+						LaeuferV.push_back(x);
+					}
+				}
+			}
+		}
+		vector<Feld> felder;
+		for (string& str : LaeuferV) {
+			felder.push_back(s.get_Spielstand()[str]);
+		}
 	return {};
 }
--- fm1SchachOOP/Turm.cpp
+++ fm1SchachOOP/Turm.cpp
@@ -15,23 +15,26 @@ vector<Feld> Turm::erlaubteFelderBerechnen(string bezeichnung)
 	{
 		for(int k = 7; k >= 0; k--)
 		{
-		if (bezeichnung.at(0) == (65 + i))
+	// Berechnung der horizontal erlaubten Felder		// erster durchlauf der aeusseren Schleife								//letzter durchlauf der aeusseren Schleife
+		if (bezeichnung.at(0) == (65 + i))				// wenn Spielfigur auf A + 0 = A										// wenn Spielfigur auf A + 7 = H
 			{
-			string x = "  ";
-				x.at(0) = bezeichnung.at(0) + k - i;
-				x.at(1) = bezeichnung.at(1);
-				if (x.at(0) != (65 + i))
-				{
+			string x = "  ";							// erster durchgang der inneren // letzte durchgang der inneren			// erster durchgang der inneren // letzte durchgang der inneren
+				x.at(0) = bezeichnung.at(0) + k - i;	// A + 7 - 0 = H		 		// A + 0 - 0 = A 						// H + 7 - 7 = H		 		// H + 0 - 7 = A 
+				x.at(1) = bezeichnung.at(1);			// Zahl bleibt unverändert
+				if (x.at(0) != (65 + i))				// wenn die ausgerechnete zahl also A + k - i == A + i entspricht, dann nicht ausgeben  
+				{										// damit wird die Position excludiert, auf der die Figur steht
 					TurmV.push_back(x);  
 				}
 			}
-			if (bezeichnung.at(1) == (49 + i))
+		// Berechnung der vertikal erlaubten Felder		// erster durchlauf der aeusseren Schleife								//letzter durchlauf der aeusseren Schleife
+			if (bezeichnung.at(1) == (49 + i))			// wenn Spielfigur auf 1 + 0 = A										// wenn Spielfigur auf 1 + 7 = 8
 			{
-				string x = "  ";
-				x.at(0) = bezeichnung.at(0) ;
-				x.at(1) = bezeichnung.at(1) + k - i;
-				if (x.at(1) != (49 + i))
-				{
+				string x = "  ";						// erster durchgang der inneren // letzte durchgang der inneren			// erster durchgang der inneren // letzte durchgang der inneren
+				x.at(0) = bezeichnung.at(0) ;			// Buchstabe bleibt unverändert
+				x.at(1) = bezeichnung.at(1) + k - i;	// 1 + 7 - 0 = 8		 		// 1 + 0 - 0 = 1 						// 8 + 7 - 7 = 8		 		// 8 + 0 - 7 = 1 
+				if (x.at(1) != (49 + i))				// wenn die ausgerechnete zahl also 1 + k - i == 1 + i entspricht, dann nicht ausgeben 
+				{										// damit wird die Position excludiert, auf der die Figur steht
+												
 					TurmV.push_back(x);
 				}
 			}
