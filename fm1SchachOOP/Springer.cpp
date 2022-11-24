783091ea68f5b11ca36893c243ae4a11ef8f388d
larbaer <lorbaer@posteo.de>
2022-11-24 12:22:12 +0100

    Doku in progress

--- fm1SchachOOP/Springer.cpp
+++ fm1SchachOOP/Springer.cpp
@@ -16,13 +16,14 @@ std::vector<Feld> Springer::erlaubteFelderBerechnen(std::string bezeichnung)
 	|| c - 2 & d + 1 || c - 2 & d - 1
 	|| d + 2 & c + 1 || d + 2 & c - 1
 	|| d + 2 & c + 1 || d + 2 & c - 1 */
-
+	// Documentation
+	/*	wenn von einer Pos*/
 	// Calculation
 	vector<string>springerV;
 
-	for (int i = 0; i < 2; i++) {	
+	for(int i : { 0 , 1 } ) {
 		if (bezeichnung.at(0) < 'G' && bezeichnung.at(1) > ('0' + i) && bezeichnung.at(1) < ('8' + i))
-		{
+		{// Right : Up and Down
 			string x = "  ";
 			x.at(0) = bezeichnung.at(0) + 2;
 			x.at(1) = bezeichnung.at(1) + (1 - (i + i));
@@ -33,7 +34,7 @@ std::vector<Feld> Springer::erlaubteFelderBerechnen(std::string bezeichnung)
 			}
 		}
 		if (bezeichnung.at(1) < '7' && bezeichnung.at(0) >= ('A' + i) && bezeichnung.at(0) < ('H' + i))
-		{
+		{// Up : Left and Right
 			string x = "  ";
 			x.at(0) = bezeichnung.at(0) + (1 - (i + i));
 			x.at(1) = bezeichnung.at(1) + 2;
@@ -43,8 +44,9 @@ std::vector<Feld> Springer::erlaubteFelderBerechnen(std::string bezeichnung)
 				springerV.push_back(x);
 			}
 		}
+		
 		if (bezeichnung.at(0) > 'B' && bezeichnung.at(1) > ('0' + i) && bezeichnung.at(1) < ('8' + i))
-		{
+		{// Left : Up and Down
 			string x = "  ";
 			x.at(0) = bezeichnung.at(0) - 2;
 			x.at(1) = bezeichnung.at(1) + (1 - (i + i));
@@ -55,7 +57,7 @@ std::vector<Feld> Springer::erlaubteFelderBerechnen(std::string bezeichnung)
 			}
 		}
 		if (bezeichnung.at(1) > '2' && bezeichnung.at(0) >= ('A' + i) && bezeichnung.at(0) < ('H' + i))
-		{
+		{// Down : Left and Right
 			string x = "  ";
 			x.at(0) = bezeichnung.at(0) + (1 - (i + i));
 			x.at(1) = bezeichnung.at(1) - 2;
@@ -67,7 +69,7 @@ std::vector<Feld> Springer::erlaubteFelderBerechnen(std::string bezeichnung)
 		}
 	}
 	
-	/* Convert erlaubteFelder into vector<Feld> */
+	/* Convert vector<string> into vector<Feld> */
 	vector<Feld> felder;
 	for (string& str : springerV)
 	{
