5eae961543200847fff7d10cd718b18648ac2afe
Brooca <53477954+Brooca@users.noreply.github.com>
2022-11-24 08:43:58 +0100

    läufer gerüst

--- fm1SchachOOP/Laeufer.cpp
+++ fm1SchachOOP/Laeufer.cpp
@@ -11,32 +11,31 @@ vector<Feld> Laeufer::erlaubteFelderBerechnen(string bezeichnung)
 {
 		// Calculation
 		vector<string> LaeuferV;
-		for (int i = 0; i < 8; i++)
+
+		for (int j = 0; j < 8; j++)
 		{
-			for (int k = 7; k >= 0; k--)
+			for (int n = 7; n >= 0; n--)
 			{
-				if (bezeichnung.at(0) == (65 + i))
+				for (int i = 0; i < 8; i++)
 				{
-					string x = "  ";
-					x.at(0) = bezeichnung.at(0) + k - i;
-					x.at(1) = bezeichnung.at(1);
-					if (x.at(0) != (65 + i))
+					for (int k = 7; k >= 0; k--)
 					{
-						LaeuferV.push_back(x);
-					}
-				}
-				if (bezeichnung.at(1) == (49 + i))
-				{
-					string x = "  ";
-					x.at(0) = bezeichnung.at(0);
-					x.at(1) = bezeichnung.at(1) + k - i;
-					if (x.at(1) != (49 + i))
-					{
-						LaeuferV.push_back(x);
+						if (abs(bezeichnung.at(0) - bezeichnung.at(1)) == abs(bezeichnung.at(0 + k - i) - bezeichnung.at(1 + n - j)))
+						{
+							string x = "  ";
+							x.at(0) = bezeichnung.at(0) + k - i;
+							x.at(1) = bezeichnung.at(1) + n - j;
+								if (x.at(0) != (65 + i))
+								{
+									LaeuferV.push_back(x);
+								}
+						}
+						
 					}
 				}
 			}
 		}
+		
 		vector<Feld> felder;
 		for (string& str : LaeuferV) {
 			felder.push_back(s.get_Spielstand()[str]);
