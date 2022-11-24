3df2000b50b0495415ece2c3ff115553a7bce87f
Brooca <53477954+Brooca@users.noreply.github.com>
2022-11-24 13:26:38 +0100

    kurz

--- fm1SchachOOP/Laeufer.cpp
+++ fm1SchachOOP/Laeufer.cpp
@@ -18,16 +18,18 @@ vector<Feld> Laeufer::erlaubteFelderBerechnen(string bezeichnung)
 				//{
 
 					// Rechts hoch
+		for(int p = 0; p < 2; p++)
+		{
 					for (int i = 1; i < 8; i++)
 					{
 						for (int k = 1; k < 8; k++)
 						{
-							if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
+							if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i - (i * p * 2)) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k - (k * p * 2)))
 							{
 
 								string x = "  ";
-								x.at(0) = bezeichnung.at(0) + i;
-								x.at(1) = bezeichnung.at(1) + k;
+								x.at(0) = bezeichnung.at(0) + i - (i * p * 2);
+								x.at(1) = bezeichnung.at(1) + k - (k * p * 2);
 									
 										
 											
@@ -55,56 +57,21 @@ vector<Feld> Laeufer::erlaubteFelderBerechnen(string bezeichnung)
 							}
 						}
 					}
-					// Links Runter
-					for (int i = 1; i < 8; i++)
-					{
-						for (int k = 1; k < 8; k++)
-						{
-							if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k))
-							{
-
-								string x = "  ";
-								x.at(0) = bezeichnung.at(0) - i;
-								x.at(1) = bezeichnung.at(1) - k;
-
-
-
-
-								if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
-								{
-									if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
-									{
-										LaeuferV.push_back(x);
-
-									}
-									else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
-									{
-										break;
-									}
-									else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
-									{
-										LaeuferV.push_back(x);
-										break;
-									}
-								}
-
-
-
-							}
-						}
-					}
+		}
+				
 					// Rechts runter
+		for (int n = 0; n < 2; n++)
+		{
 					for (int i = 1; i < 8; i++)
 					{
 						for (int k = 1; k < 8; k++)
 						{
-							if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k))
+							if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i - (i * n * 2)) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k + (k * n * 2)))
 							{
 
 								string x = "  ";
-								x.at(0) = bezeichnung.at(0) + i;
-								x.at(1) = bezeichnung.at(1) - k;
-
+								x.at(0) = bezeichnung.at(0) + i - (i * n * 2);
+								x.at(1) = bezeichnung.at(1) - k + (k * n * 2);
 
 
 
@@ -131,44 +98,8 @@ vector<Feld> Laeufer::erlaubteFelderBerechnen(string bezeichnung)
 							}
 						}
 					}
-					// Links runter
-					for (int i = 1; i < 8; i++)
-					{
-						for (int k = 1; k < 8; k++)
-						{
-							if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
-							{
-
-								string x = "  ";
-								x.at(0) = bezeichnung.at(0) - i;
-								x.at(1) = bezeichnung.at(1) + k;
-
-
-
-
-								if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
-								{
-									if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
-									{
-										LaeuferV.push_back(x);
-
-									}
-									else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
-									{
-										break;
-									}
-									else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
-									{
-										LaeuferV.push_back(x);
-										break;
-									}
-								}
-
-
-
-							}
-						}
-					}
+		}
+					
 		vector<Feld> felder;
 		for (string& str : LaeuferV) {
 			felder.push_back(s.get_Spielstand()[str]);
