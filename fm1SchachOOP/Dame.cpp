fb9c5eb632cb2cd39d56cb2f2727da78e736318a
larbaer <lorbaer@posteo.de>
2022-11-25 09:50:15 +0100

    wo erstellen

--- fm1SchachOOP/Dame.cpp
+++ fm1SchachOOP/Dame.cpp
@@ -10,7 +10,9 @@ vector<Feld> Dame::erlaubteFelderBerechnen(std::string bezeichnung)
 {
 	// Calculation
 	//vector<Feld> DameV
+	// Turm erstellen
 	// DameV <- Turm erlaubte Felder
+	// Laeufer erstellen
 	// DameV <- Laeufer
 	// return dameV
 }
