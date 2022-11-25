42a6a4069362b2037065deab677f95c1df117cfe
larbaer <lorbaer@posteo.de>
2022-11-25 09:10:33 +0100

    Laeufer update to return value

--- fm1SchachOOP/Laeufer.cpp
+++ fm1SchachOOP/Laeufer.cpp
@@ -12,209 +12,217 @@ vector<Feld> Laeufer::erlaubteFelderBerechnen(string bezeichnung)
 	// Calculation
 	vector<string> LaeuferV;
 
-	
-			// Rechts hoch
+
+	// Rechts hoch
 	bool erlaubnis = true;
-	
-		for (int i = 1; i < 8; i++)
-		{ 
-			if (erlaubnis == true) 
-			{
 
-			
-				for (int k = 1; k < 8; k++)
-				{
-					
-						if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
-						{
-
-							string x = "  ";
-							x.at(0) = bezeichnung.at(0) + i;
-							x.at(1) = bezeichnung.at(1) + k;
-
-
-
-
-							if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
-							{
-								if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
-								{
-									LaeuferV.push_back(x);
-
-								}
-								else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
-								{
-									break;
-								}
-								else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
-								{
-
-									LaeuferV.push_back(x);
-									erlaubnis = false;
-									break;
-								}
-							}
-						}
-					
-				}
-			}
-			else 
-			{
-				break;
-			}
-		}
-	
-
-				
-			
-		
-		// Links Runter
-		 erlaubnis = true;
-
-		for (int i = 1; i < 8; i++)
+	for (int i = 1; i < 8; i++)
+	{
+		if (erlaubnis == true)
 		{
-			if (erlaubnis == true)
+
+
+			for (int k = 1; k < 8; k++)
 			{
 
-				for (int k = 1; k < 8; k++)
+				if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
 				{
-					if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k))
+
+					string x = "  ";
+					x.at(0) = bezeichnung.at(0) + i;
+					x.at(1) = bezeichnung.at(1) + k;
+
+
+
+
+					if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
 					{
-
-						string x = "  ";
-						x.at(0) = bezeichnung.at(0) - i;
-						x.at(1) = bezeichnung.at(1) - k;
-
-
-
-
-						if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+						if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
 						{
-							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
-							{
-								LaeuferV.push_back(x);
+							LaeuferV.push_back(x);
 
-							}
-							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
-							{
-								break;
-							}
-							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
-							{
-								LaeuferV.push_back(x);
-								erlaubnis = false;
-								break;
-							}
 						}
+						else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							break;
+						}
+						else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
 
-
-
+							LaeuferV.push_back(x);
+							erlaubnis = false;
+							break;
+						}
 					}
 				}
-			}
-			else 
-			{
-				break;
+
 			}
 		}
-
-		// Rechts runter
-
-		 erlaubnis = true;
-
-		for (int i = 1; i < 8; i++)
+		else
 		{
-			if (erlaubnis == true) 
+			break;
+		}
+	}
+
+
+
+
+
+	// Links Runter
+	erlaubnis = true;
+
+	for (int i = 1; i < 8; i++)
+	{
+		if (erlaubnis == true)
+		{
+
+			for (int k = 1; k < 8; k++)
 			{
-
-				for (int k = 1; k < 8; k++)
+				if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k))
 				{
-					if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k))
+
+					string x = "  ";
+					x.at(0) = bezeichnung.at(0) - i;
+					x.at(1) = bezeichnung.at(1) - k;
+
+
+
+
+					if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
 					{
-
-						string x = "  ";
-						x.at(0) = bezeichnung.at(0) + i;
-						x.at(1) = bezeichnung.at(1) - k;
-
-
-
-
-						if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+						if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
 						{
-							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
-							{
-								LaeuferV.push_back(x);
+							LaeuferV.push_back(x);
 
-							}
-							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
-							{
-								break;
-							}
-							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
-							{
-								LaeuferV.push_back(x);
-								erlaubnis = false;
-								break;
-							}
 						}
-
-
-
+						else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							break;
+						}
+						else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							LaeuferV.push_back(x);
+							erlaubnis = false;
+							break;
+						}
 					}
+
+
+
 				}
 			}
-			else 
-			{
-				break;
-			}
 		}
-
-		// Links runter
-
-		 erlaubnis = true;
-
-		for (int i = 1; i < 8; i++)
+		else
 		{
-			if (erlaubnis == true) {
+			break;
+		}
+	}
 
-				for (int k = 1; k < 8; k++)
+	// Rechts runter
+
+	erlaubnis = true;
+
+	for (int i = 1; i < 8; i++)
+	{
+		if (erlaubnis == true)
+		{
+
+			for (int k = 1; k < 8; k++)
+			{
+				if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) - k))
 				{
-					if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
+
+					string x = "  ";
+					x.at(0) = bezeichnung.at(0) + i;
+					x.at(1) = bezeichnung.at(1) - k;
+
+
+
+
+					if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
 					{
-
-						string x = "  ";
-						x.at(0) = bezeichnung.at(0) - i;
-						x.at(1) = bezeichnung.at(1) + k;
-
-
-
-
-						if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+						if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
 						{
-							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
-							{
-								LaeuferV.push_back(x);
+							LaeuferV.push_back(x);
 
-							}
-							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
-							{
-								break;
-							}
-							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
-							{
-								LaeuferV.push_back(x);
-								erlaubnis = false;
-								break;
-							}
 						}
-
-
-
+						else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							break;
+						}
+						else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							LaeuferV.push_back(x);
+							erlaubnis = false;
+							break;
+						}
 					}
+
+
+
 				}
 			}
-			else
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	// Links runter
+
+	erlaubnis = true;
+
+	for (int i = 1; i < 8; i++)
+	{
+		if (erlaubnis == true) {
+
+			for (int k = 1; k < 8; k++)
 			{
-				break;
+				if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
+				{
+
+					string x = "  ";
+					x.at(0) = bezeichnung.at(0) - i;
+					x.at(1) = bezeichnung.at(1) + k;
+
+
+
+
+					if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+					{
+						if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
+						{
+							LaeuferV.push_back(x);
+
+						}
+						else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							break;
+						}
+						else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
+						{
+							LaeuferV.push_back(x);
+							erlaubnis = false;
+							break;
+						}
+					}
+
+
+
+				}
 			}
 		}
+		else
+		{
+			break;
+		}
+	}
+	vector<Feld> felder;
+	for (string& str : LaeuferV)
+	{
+		felder.push_back(s.get_Spielstand()[str]);
+	}
+
+	return felder;
+}
