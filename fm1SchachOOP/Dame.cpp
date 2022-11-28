447ad537e22905ad7166b8baca17e3d5cfafe0e2
larbaer <lorbaer@posteo.de>
2022-11-28 23:02:21 +0100

    Documentation Dame

--- fm1SchachOOP/Dame.cpp
+++ fm1SchachOOP/Dame.cpp
@@ -8,28 +8,27 @@ Dame::Dame(bool symbol)
 
 vector<Feld> Dame::erlaubteFelderBerechnen(std::string bezeichnung)
 {
-	// Calculation
-	vector<Feld> DameV;
-	vector<Feld> zwischenschritt;
-	// Datentyp Name (parameter)
-	Turm DameTurm(this->farbe)  ;
+	// Documentation
+	/* Es werden jeweils die Felder berechnet als wuerden an der Stelle ein Turm beziehungsweise ein Laeufer stehen.
+		- hierzu werden diese Figuren konstruiert und deren Berechnung durchgefuehrt
+		- die ergebnisse werden dem vector<Feld> DameV hinzugefuegt */
 
-
-	DameV = DameTurm.erlaubteFelderBerechnen(bezeichnung);
+	// Variables
+	vector<Feld> DameV, TurmV, LaeuferV;
 	
-	Laeufer DameLaeufer(this->farbe);
+	// Dummy Figuren
+	Turm Turm(this->farbe);
+	Laeufer Laeufer(this->farbe);
 
-	zwischenschritt = DameLaeufer.erlaubteFelderBerechnen(bezeichnung);
+	TurmV = Turm.erlaubteFelderBerechnen(bezeichnung);
+	LaeuferV = Laeufer.erlaubteFelderBerechnen(bezeichnung);
 
-	for (auto& feld : zwischenschritt )
-	{
-		DameV.push_back(feld);
-	}
-	
+	// Adding Turm and Laeufer into DameV
+	for (auto& feld : TurmV) { DameV.push_back(feld); }
+	for (auto& feld : LaeuferV) {	DameV.push_back(feld);	}
 
-
+	// Return
 	return DameV;
-
 }
 
 
--- fm1SchachOOP/Spiel.cpp
+++ fm1SchachOOP/Spiel.cpp
@@ -7,7 +7,7 @@ Spiel::Spiel()
 	set_Spieler();
 
 	// Key-String						   X_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-X
-	string figurenFolge = "TSLDKLSTBBBBBBBB                                bbbbbbbbbtsldklst";
+	string figurenFolge = "TSLDKLST BBBBBBB             D                  bbbbbbbbbtsldklst";
  // string figurenFolge = "TSLDKLSTBBBBBBBB                                bbbbbbbbtsldklst";
 	int index = 0;
 
