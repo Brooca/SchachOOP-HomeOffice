cb1233aa2a34f39c7c6416913fe6f3eefa266fac
Brooca <53477954+Brooca@users.noreply.github.com>
2022-11-28 10:22:28 +0100

    König ->Turm

--- fm1SchachOOP/Koenig.cpp
+++ fm1SchachOOP/Koenig.cpp
@@ -10,41 +10,40 @@ Koenig::Koenig(bool symbol)
 vector<Feld> Koenig::erlaubteFelderBerechnen(string bezeichnung)
 {
 
-	// Calculation
+	// Calculation Horizontal/ Vertikal
 	vector<string> KoenigV;
-	for (int m = 0; m < 2; m++)						// 0 = horizonal; 1 = vertical
+	for (int m = 0; m < 2; m++)						// 0 = horizonal; 1 = vertikal
 	{
 		for (int n = 0; n < 3; n += 2)				// 0 = Horizontal Links & vertikal runter; 2 = Horizontal rechts & vertikal hoch
 		{
 			for (int i = 0; i < 8; i++)				
 			{
-				for (int k = 0; k < 8; k++)
-				{
+				
 					// Berechnung der erlaubten Felder 								
-					if (bezeichnung.at(m) == (65 + i - (16 * m)))													// Feld auf dem man steht				| 65 + 0 = A 		
+					if (bezeichnung.at(m) == (65 + i - (16 * m)))			// m = 0; i = 2; C									// Feld auf dem man steht				| 65 + 0 = A 		
 					{
 						string x = "  ";
-						x.at(m) = bezeichnung.at(m) + k - (k * n);												// Feld auf das man sich bewegen kann	| 65 + 0 = A 		
-						x.at(1 - m) = bezeichnung.at(1 - m);														// vertikale / horizontale bleibt unverändert		
-						if (x.at(m) != (65 + i - (16 * m)) && x.at(m) >= 65 - (16 * m) && x.at(m) <= 72 - (16 * m)) // Felder auf dem man steht excludieren & nur on Board range erlauben		
-						{
-							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')						// Wenn das Feld leer ist, ist es erlaubt -> pushback
+						x.at(m) = bezeichnung.at(m) + 1 - (1 * n);			//schreiben in erste stelle des string -> C + 1	= D -> D wird an erste stelle geschrieben											
+						x.at(1 - m) = bezeichnung.at(1 - m);				// 5 bleibt 5 -> kommt an zweite stelle																	
+						if (x.at(m) != (65 + i - (16 * m)) && x.at(m) >= 65 - (16 * m) && x.at(m) <= 72 - (16 * m))								// Felder auf dem man steht excludieren & nur on Board range erlauben		
+						{//  D		!=  C				   && D		  >= A			   && D		  <= H	
+							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')													// Wenn das Feld leer ist, ist es erlaubt -> pushback
 							{
 								KoenigV.push_back(x);
 
 							}
-							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())					// Wenn die Figur auf dem Feld die selbe Farbe hat als diese Figur kein pushback & break
+							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())												// Wenn die Figur auf dem Feld die selbe Farbe hat als diese Figur kein pushback & break
 							{
 								break;
-							}
-							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())					// Wenn die Figur auf dem Feld eine andere Farbe hat als diese Figur, ist es erlaubt -> pushback & danach break
-							{
+							}		
+							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())												// Wenn die Figur auf dem Feld eine andere Farbe hat als diese Figur, ist es erlaubt -> pushback & danach break
+							{		
 								KoenigV.push_back(x);
 								break;
 							}
 						}
 					}
-				}
+				
 			}
 		}
 	}
