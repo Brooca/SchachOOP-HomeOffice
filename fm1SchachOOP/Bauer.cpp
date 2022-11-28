c9017eec31eb9dfd1581d200b621a48e96206a6b
Brooca <53477954+Brooca@users.noreply.github.com>
2022-11-28 11:41:53 +0100

    merge

--- fm1SchachOOP/Bauer.cpp
+++ fm1SchachOOP/Bauer.cpp
@@ -32,22 +32,22 @@ vector<Feld> Bauer::erlaubteFelderBerechnen(string bezeichnung)
 			}
 		}
 
-			// b läuft von startposition 2 runter
-		if (bezeichnung.at(1) == 55 && s.get_Spielstand()[bezeichnung.at(0),(bezeichnung.at(1))+1].get_Figur()->get_Bezeichnung() == '  ')
-		{
-			string x = "  ";
+		// doppelter Schritt zum Anfang
+		if (bezeichnung.at(0) >= 'A' && bezeichnung.at(0) <= 'H' && bezeichnung.at(1) == '7') {
 			x.at(0) = bezeichnung.at(0);
-			x.at(1) = bezeichnung.at(1) - 2;
+			x.at(1) = bezeichnung.at(1) - 1;
+			// Freeway : wenn ja : Feld hinzufuegen
+			if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
+			{
+				BauerV.push_back(x);
+
+				// Zweiter Schritt
+				x.at(1) = bezeichnung.at(1) - 2;
 				if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
 				{
 					BauerV.push_back(x);
-					break;
 				}
-			
-				else
-				{
-					break;
-				}
+			}
 		}
 			// b schlägt rechts runter
 		if(bezeichnung.at(1) != 49 && bezeichnung.at(0) <72 )
