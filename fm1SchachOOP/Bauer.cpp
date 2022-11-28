c1b494c789637b62768507ea1c2d69a167be2955
larbaer <lorbaer@posteo.de>
2022-11-28 10:58:10 +0100

    Bauer unten laeuft eins hoch

--- fm1SchachOOP/Bauer.cpp
+++ fm1SchachOOP/Bauer.cpp
@@ -1,5 +1,7 @@
 #include "Bauer.h"
 
+extern Spiel s;
+
 Bauer::Bauer(bool symbol)
 {
 	farbe = symbol;
@@ -9,18 +11,35 @@ Bauer::Bauer(bool symbol)
 vector<Feld> Bauer::erlaubteFelderBerechnen(string bezeichnung)
 {
 	vector<string> BauerV;
-
+	string x	= "  ";
 	switch (this->farbe) {
 	case 0: // b läuft runter
-		string x	= "  ";
-		x.at(0)		= bezeichnung.at(0);
-		x.at(1)		 = bezeichnung.at(1) + 1;
+
+		x.at(0) = bezeichnung.at(0);
+		x.at(1) = bezeichnung.at(1) + 1;
 		break;
 
 	case 1: // B läuft hoch
+		x.at(0) = bezeichnung.at(0);
+		x.at(1) = bezeichnung.at(1) + 1;
 
-		break;
+		if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+		{
+			if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
+			{
+				BauerV.push_back(x);
+
+			}
 		}
-	return {};
+		break;
+	}	
+	// Convert vector<string> into vector<Feld>
+	vector<Feld> felder;
+	for (string& str : BauerV)
+	{
+		felder.push_back(s.get_Spielstand()[str]);
+	}
+	// Return
+	return felder;
 }
 
