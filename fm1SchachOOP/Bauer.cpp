2db341681645b8edf1f540a550a1db88e08bb4f1
larbaer <lorbaer@posteo.de>
2022-11-28 11:33:47 +0100

    Bauer weis fertig

--- fm1SchachOOP/Bauer.cpp
+++ fm1SchachOOP/Bauer.cpp
@@ -24,8 +24,8 @@ vector<Feld> Bauer::erlaubteFelderBerechnen(string bezeichnung)
 		// einfacher Schritt ab linie 3
 		if (bezeichnung.at(0) >= 'A' && bezeichnung.at(0) <= 'H' && bezeichnung.at(1) >= '3' && bezeichnung.at(1) <= '8')
 		{
-		x.at(0) = bezeichnung.at(0);
-		x.at(1) = bezeichnung.at(1) + 1;
+			x.at(0) = bezeichnung.at(0);
+			x.at(1) = bezeichnung.at(1) + 1;
 			if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
 			{
 				BauerV.push_back(x);
@@ -51,14 +51,103 @@ vector<Feld> Bauer::erlaubteFelderBerechnen(string bezeichnung)
 		}
 		/*Diagonal Schlagen*/
 
-		break;
-	}	
+		// Rechts Hoch
+		bool erlaubnis = true;
+
+		for (int i = 1; i < 8; i++)
+		{
+			if (erlaubnis == true)
+			{
+				for (int k = 1; k < 8; k++)
+				{
+					if (abs(bezeichnung.at(0) - bezeichnung.at(0) + i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
+					{
+
+						string x = "  ";
+						x.at(0) = bezeichnung.at(0) + i;
+						x.at(1) = bezeichnung.at(1) + k;
+
+						if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+						{
+							if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
+							{
+								erlaubnis = false;
+								break;
+							}
+							else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
+							{
+								erlaubnis = false;
+								break;
+							}
+							else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
+							{
+
+								BauerV.push_back(x);
+								erlaubnis = false;
+								break;
+							}
+						}
+					}
+
+				}
+			}
+			else
+			{
+				break;
+			}
+
+			// Links hoch
+			erlaubnis = true;
+
+			for (int i = 1; i < 8; i++)
+			{
+				if (erlaubnis == true) {
+					for (int k = 1; k < 8; k++)
+					{
+						if (abs(bezeichnung.at(0) - bezeichnung.at(0) - i) == abs(bezeichnung.at(1) - bezeichnung.at(1) + k))
+						{
+							string x = "  ";
+							x.at(0) = bezeichnung.at(0) - i;
+							x.at(1) = bezeichnung.at(1) + k;
+
+							if (x.at(0) >= 65 && x.at(0) <= 72 && x.at(1) >= 49 && x.at(1) <= 56)
+							{
+								if (s.get_Spielstand()[x].get_Figur()->get_Bezeichnung() == ' ')
+								{
+									erlaubnis = false;
+									break;
+								}
+								else if (this->farbe == s.get_Spielstand()[x].get_Figur()->get_Farbe())
+								{
+									erlaubnis = false;
+									break;
+								}
+								else if (this->farbe != s.get_Spielstand()[x].get_Figur()->get_Farbe())
+								{
+									BauerV.push_back(x);
+									erlaubnis = false;
+									break;
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+			break;
+		}
+	}
+
 	// Convert vector<string> into vector<Feld>
 	vector<Feld> felder;
 	for (string& str : BauerV)
 	{
 		felder.push_back(s.get_Spielstand()[str]);
 	}
+
 	// Return
 	return felder;
 }
